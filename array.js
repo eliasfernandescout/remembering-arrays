7f8cdc2a2027bfe1c0ec9ca65486c0f963a31532
eliasfrnandes <eliasfernandescout@gmail.com>
2022-04-05 22:05:18 -0300

    final aula array

--- array.js
+++ array.js
@@ -1,3 +1,16 @@
-let aprovados = ['Ragnar', 'Ivvar', 'Bjorn'] //ARRAY É UMA ESTRURUA INDEXADA  PARTIR DO 0
+let aprovados = ['Ragnar', 'Ivar', 'Bjorn'] //ARRAY É UMA ESTRURUA INDEXADA  PARTIR DO 0
 console.log(aprovados[0])
 
+aprovados.push('Ubbe', 'Floki', 'Horik', 'Sigurd')
+console.log(aprovados)
+
+const iniciais = aprovados.map(function retornarInicial(inicialAlunos) {
+    return inicialAlunos[0]
+})
+console.log(iniciais)
+
+const sort = aprovados.sort()
+console.log(sort)
+
+const splice = aprovados.splice(1, 1, 'Excluir e Incluir novo elemento')
+console.log(aprovados)
